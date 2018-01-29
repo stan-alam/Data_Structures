96c1a675a9e1cae74482f2a6353e2464aaee1692
Stan Alam <stan.alam@gmail.com>
2018-01-29 07:42:05 -0500

    updated Arrayman.java

--- Java/review/Arrayman.java
+++ Java/review/Arrayman.java
@@ -13,7 +13,7 @@ public class Arrayman {
     A.set(writeIndex++, A.get(i));
    }
   }
-  return writeIndex;
+     return writeIndex;
 
  }
 
@@ -21,8 +21,37 @@ public class Arrayman {
   Arrayman am = new Arrayman();
   List < Integer > stanList = new ArrayList < Integer > ();
 
-  //int[] A  = new int[]{2,3,4,5,6,6,6,7,9};
-  am.deleteDuplicates(stanList);
+  stanList.add(1);
+  stanList.add(2);
+  stanList.add(2);
+  stanList.add(4);
+  stanList.add(5);
+  stanList.add(6);
+  stanList.add(7);
+  stanList.add(8);
+  stanList.add(9);
+
+  int    retVal;
+
+  //int[] A  = new int[]{2,3,4,5,6,6,6,7,9};  you have commented this line
+
+   /*  Calling the method without object name, for you have
+       define the method as "static"; when method is "static
+      you can call it without object name.
+
+   */
+
+
+
+    retVal = deleteDuplicates(stanList); // As the method has data type "int", you should
+                                        // call it, with an arrangement, so that it can collect
+                                        // the returned value;  you need to provide an "integer variable"
+                                        // to hold the value. Here returned value will be in "retVal"
+    System.out.println("retVal =" +retVal);
+
+    /* In the input, I put 2 two times, so it returns index = 8       */
+
+
  }
 
 }
